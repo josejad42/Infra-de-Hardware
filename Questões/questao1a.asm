d2d8a9308acdae9e4530f8085b2c2851ed889ede
Alex Carvalho <jac@cin.ufpe.br>
2023-07-17 22:48:16 -0300

    Add files via upload

---
+++ Questões/questao1a.asm
@@ -0,0 +1,46 @@
+condicao1:
+#carrega a, b, c nos registradores
+lw x5, a
+lw x6, b
+lw x7, c
+#garante como x16 (nosso x) começa zerado
+xor x16, x16, x16
+#verifica se a >= 0
+bge x5, x0, condicao2
+jal x11, fim
+
+condicao2:
+lw x28, sessenta_dois
+#verifica b<62
+blt x6, x28, condicao3a
+#verifica b==62
+beq x6, x28, condicao3a
+jal x11, fim
+
+condicao3a:
+#verifica c >= 15 (o necessario é ver se c>15, por isso a parte B)
+lw x29, quinze
+bge x7, x29, condicao3b
+jal x11, fim
+
+condicao3b:
+#verifica se c nao é igual a 15
+beq x7, x29, fim
+jal x11, change
+
+
+change:
+# muda x16 (nosso x) para 1 se todas as condicoes anteriores foram satisfeitas
+addi x16, x16, 1
+jal x11, fim
+
+fim:
+halt
+
+a: .word 0
+b: .word 62
+c: .word 15
+sessenta_dois: .word 62
+quinze: .word 15
+tres: .word 3
+
