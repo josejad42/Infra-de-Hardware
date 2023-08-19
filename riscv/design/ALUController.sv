4242d992ae5cdd633bb67811f1d03f93ffce93df
Juliana Silva <85325364+jsdana@users.noreply.github.com>
2023-08-19 20:26:03 -0300

    Update ALUController.sv

--- riscv/design/ALUController.sv
+++ riscv/design/ALUController.sv
@@ -12,6 +12,7 @@ module ALUController (
 
   assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // R\I-or
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) ||   // BNE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||   // BLT
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000)) || //R\I - slli
@@ -20,6 +21,7 @@ module ALUController (
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) ||   // BGE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||   // BLT
       ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
       ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000)) || //R\I - slli
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) || //R\I -srli
@@ -39,6 +41,7 @@ module ALUController (
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) ||   // BNE
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) ||   // BGE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||   // BLT
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) || //R\I -srli
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || //R\I - srai
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<(slt)
